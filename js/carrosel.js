4997585095201495afe4da46ba5d3ca89309f14e
Jadson Hudson <jadson.hudson77@gmail.com>
2024-06-20 16:05:10 -0300

    feat:criacao da secao conheca os personagens

---
+++ js/carrosel.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const slides = document.querySelectorAll('.personagens-info');
+    const buttons = document.querySelectorAll('#personagens-fotos img');
+
+    // Função para mostrar o slide correspondente
+    function showSlide(event) {
+        const targetId = event.target.getAttribute('data-slide');
+        console.log(targetId)
+        buttons.forEach(button => {
+            button.setAttribute("modo", button.getAttribute('data-slide') === targetId ? 'ativado' : 'desativado');
+        });
+        slides.forEach(slide => {
+            slide.style.display = slide.id === targetId ? 'flex' : 'none';
+        });
+    }
+
+    // Adiciona evento de clique aos botões
+    buttons.forEach(button => {
+        button.addEventListener('click', showSlide);
+    });
+
+    if (window.location.hash) {
+        // Exibe o slide selecionado
+        const hash = window.location.hash.slice(1);
+        showSlide({ target: { getAttribute: () => hash } });
+    } else {
+        // Exibe o primeiro slide automaticamente
+        showSlide({ target: { getAttribute: () => 'p-maria' } });
+    }
+});
